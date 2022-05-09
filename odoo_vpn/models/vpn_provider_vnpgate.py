cf55c39916e708375c24f629d0e555cd3e5a7d9e
Anh Vu <vuna2004@gmail.com>
2022-05-09 15:47:42 +0700

    [IMP] :memo: update ovpn

--- odoo_vpn/models/vpn_provider_vnpgate.py
+++ odoo_vpn/models/vpn_provider_vnpgate.py
@@ -1,8 +1,5 @@
-from pyquery import PyQuery
-
 import base64
 import requests
-from bs4 import BeautifulSoup
 
 from datetime import date, datetime, timedelta
 from odoo import api, fields, models, _
@@ -23,18 +20,59 @@ class VpnProviderVpnGate1(models.Model):
 class VpnProviderVpnGate(models.Model):
     _inherit = 'vpn.provider'
 
-
+    @api.model
     def queue_vpn_collect_data_vpngate(self, record_id):
-        print('->>> queue_vpn_collect_data_vpngate')
         record = self.search([
             ('id', '=', record_id),
             ('code', '=', 'vpngate'),
         ])
+        vpn_env = self.env['vpn.vpn']
         if record:
-            url = '%s/en' % (record.base_url)
-            html = self._get_url(url)
-            if html is not None:
-                pq = PyQuery(html)
-                print()
+            try:
+                vpn_data = requests.get(record.base_url).text.replace('\r','')
+                servers = [line.split(',') for line in vpn_data.split('\n')]
+                servers = [s for s in servers[2:] if len(s) > 1]
+                for server in servers:
+                    host_name = server[0]
+                    ip = server[1]
+                    score = server[2]
+                    ping = server[3]
+                    speed = server[4]
+                    country_long = server[5]
+                    country_short = server[6]
+                    num_vpn_sessions = server[7]
+                    uptime = server[8]
+                    total_user = server[9]
+                    total_traffic = server[10]
+                    log_type = server[11]
+                    operator = server[12]
+                    message = server[13]
+                    openvpn_config_base64 = server[14]
+                    # tcp_port = server[15]
+                    # udp_port = server[16]
+                    # l2tp = server[17]
+                    #HostName,IP,Score,Ping,Speed,CountryLong,CountryShort,NumVpnSessions,Uptime,TotalUsers,TotalTraffic,LogType,Operator,Message,OpenVPN_ConfigData_Base64
+                    vpn_recs = vpn_env.search([('name', '=', host_name)])
+                    if not vpn_recs:
+                        vpn_env.create({
+                            'name': host_name,
+                            'ip': ip,
+                            'score': score,
+                            'ping': ping,
+                            'speed': float(speed)/1000000, #Mbps
+                            'country_long': country_long,
+                            'country_short': country_short,
+                            'num_vpn_sessions': num_vpn_sessions,
+                            'uptime': uptime,
+                            'total_user': total_user,
+                            'total_traffic': float(total_traffic)/1000000000, #GB,
+                            'log_type': log_type,
+                            'operator': operator,
+                            'message': message,
+                            'openvpn_config_base64': openvpn_config_base64,
+                            'provider_id': record_id,
+                        })
+            except Exception as e:
+                return e
         return True
 
--- odoo_vpn/models/vpn_vpn.py
+++ odoo_vpn/models/vpn_vpn.py
@@ -1,12 +1,12 @@
-
 import base64
-import requests
-from bs4 import BeautifulSoup
 
 from datetime import date, datetime, timedelta
 from odoo import api, fields, models, _
+from odoo.modules.module import get_module_path
+
 from odoo.tools.misc import DEFAULT_SERVER_DATE_FORMAT as DF,\
     DEFAULT_SERVER_DATETIME_FORMAT as DTF
+
 
 import logging
 
@@ -19,7 +19,37 @@ class VpnVpn(models.Model):
     _description = _('VPN')
     _order = 'create_date desc'
 
+    provider_id = fields.Many2one('vpn.provider', required=True)
     name = fields.Char(required=True)
+    ip = fields.Char()
+    score = fields.Char()
+    ping = fields.Char()
+    speed = fields.Float()
+    country_long = fields.Char()
+    country_short = fields.Char()
+    num_vpn_sessions = fields.Char()
+    uptime = fields.Char()
+    total_user = fields.Char()
+    total_traffic = fields.Float()
+    log_type = fields.Char()
+    operator = fields.Char()
+    message = fields.Char()
+    openvpn_config_base64 = fields.Char()
+    active = fields.Boolean(default=True)
+
+    def btn_download(self):
+        self.ensure_one()
+        module_path = get_module_path('odoo_vpn')
+        file_name = '%s.ovpn' % (self.name)
+        file_path = '%s/static/src/download/%s' % (module_path, file_name)
+        f = open(file_path, 'w')
+        f.write(base64.b64decode(self.openvpn_config_base64).decode('utf-8'))
+        f.close()
+        return {
+            'type': 'ir.actions.act_url',
+            'url': str('odoo_vpn/static/src/download/%s' % (file_name)),
+            'target': 'new',
+        }
 
     def cron_vpn_collect_data(self):
         self.env['vpn.provider'].vpn_collect_data()
